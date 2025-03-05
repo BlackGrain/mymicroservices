2fabe0105e1493f27c0cd26b9f0aeb57bda0172d
yancong05 <yancong05@meituan.com>
2025-03-05 12:11:28 +0800

    创建结构体

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BlackGrain/mymicroservices
+
+go 1.23.3
---
+++ internal/application/domain/order.go
@@ -0,0 +1,25 @@
+package domain
+
+import "time"
+
+type ProductItem struct {
+	ProductId string  `json:"product_id"`
+	UnitPrice float32 `json:"unit_price"`
+	Quantity  int32   `json:"quantity"`
+}
+type Order struct {
+	ID         string        `json:"id"`
+	CustomerId string        `json:"customer_id"`
+	Status     string        `json:"status"`
+	OrderItems []ProductItem `json:"order_items"`
+	CreatedAt  time.Time     `json:"created_at"`
+}
+
+func NewOrder(customerId string, items []ProductItem) *Order {
+	return &Order{
+		CustomerId: customerId,
+		Status:     "pending",
+		OrderItems: items,
+		CreatedAt:  time.Now(),
+	}
+}
